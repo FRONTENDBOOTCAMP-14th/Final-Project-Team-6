c6f65bab4ce4e823e394fc68ab97292df3b2030f
hunky1st <hunky1st@naver.com>
2025-10-02 15:14:49 +0900

    [fix] : 설정 아이콘 우측 끝 잘림 현상viewBox 비율원본 조절을 통해 수정

--- src/components/icons/icon-settings.tsx
+++ src/components/icons/icon-settings.tsx
@@ -3,7 +3,7 @@ export default function IconSettings({ color = "white", size = 24 }: IconType) {
   return (
     <svg
       width={size}
-      viewBox="0 0 24 22"
+      viewBox="0 0 26 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       aria-hidden="true"
