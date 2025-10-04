58467727cfbb598c28b7b516a1ad9cfeb830c7e7
Jandi95 <dduk2684@naver.com>
2025-10-04 20:32:14 +0900

    [feat] : 전역으로 사용할 모달 Modal Dialog 컴포넌트 개발

---
+++ src/components/dialogs/confirm-dialog.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { useDialog } from "@/stores/use-dialog";
+import { siteHexColor } from "@/utils/site-hex-color";
+import Button from "../common/button";
+import IconCheck from "../common/icons/icon-check";
+import IconClose from "../common/icons/icon-close";
+
+export default function ConfirmDialog() {
+  const { message, onConfirm, closeDialog } = useDialog();
+
+  const handleDialogConfirm = () => {
+    onConfirm?.();
+    closeDialog();
+  };
+
+  return (
+    <div>
+      <DialogTitle className="text-2xl font-bold mb-6">확인</DialogTitle>
+      <p className="text-site-gray mb-10">{message}</p>
+      <div className="flex gap-4 items-center justify-center">
+        <Button type="button" onClick={handleDialogConfirm}>
+          확인
+          <IconCheck />
+        </Button>
+        <Button
+          type="button"
+          onClick={closeDialog}
+          buttonColor={siteHexColor.gray}
+        >
+          취소
+          <IconClose />
+        </Button>
+      </div>
+    </div>
+  );
+}
