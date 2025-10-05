67a43a8d9848c497f930d018f896b31a04f8fa0c
hunky1st <hunky1st@naver.com>
2025-10-05 13:19:17 +0900

    [refactor] : 코드 단순화 및 스타일링 방식 통일

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,33 +1,14 @@
 import Link from "next/link";
-import { siteHexColor } from "@/utils/site-hex-color";
 
-// 흔적기관 + 1 입니다.
-interface FooterProps {
-  className?: string;
-}
-
-export default function Footer({ className }: FooterProps) {
-  function getCurrentYear() {
-    return new Date().getFullYear();
-  }
-  const currentYear = getCurrentYear();
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
 
   return (
-    <footer
-      className={`w-full bottom-0 fixed z-50 ${className}`}
-      style={{ backgroundColor: siteHexColor.black }}
-    >
-      <div
-        className="w-full h-[121px] px-[20px] pt-[80px] pb-[20px] flex justify-between items-center text-sm"
-        style={{ color: siteHexColor.gray }}
-      >
+    <footer className="w-full bg-[var(--color-site-black)]">
+      <div className="w-full h-[121px] px-[20px] pt-[80px] pb-[20px] flex justify-between items-center text-sm text-[var(--color-site-gray)]">
         <div className="flex justify-between items-center gap-[16px]">
-          <Link href="/privacy" className="">
-            개인정보 처리방침
-          </Link>
-          <Link href="/terms" className="">
-            이용약관
-          </Link>
+          <Link href="/privacy">개인정보 처리방침</Link>
+          <Link href="/terms">이용약관</Link>
         </div>
         <p>&copy; {currentYear} Eyepath.</p>
       </div>
