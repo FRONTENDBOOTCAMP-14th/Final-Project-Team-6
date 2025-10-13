c5e1798926e8b3d5803da3588e33aa08058e8832
hunky1st <hunky1st@naver.com>
2025-10-13 13:23:11 +0900

    [feat] : write 페이지 폼 구조 생성

--- src/app/post/write/page.tsx
+++ src/app/post/write/page.tsx
@@ -1,3 +1,35 @@
-export default function ApplyPage() {
-  return <div>동행 신청 페이지</div>;
+import { createPost } from "@/app/post/action";
+import { Button, Input } from "@/components/common/index";
+
+export default function PostWritePage() {
+  return (
+    <main className="w-full max-w-md mx-auto p-4">
+      <h1 className="text-2xl font-bold text-center mb-6">동행 신청 페이지</h1>
+
+      <form action={createPost} className="flex flex-col gap-5">
+        <Input
+          label="제목"
+          name="title"
+          type="text"
+          placeholder="제목을 입력해주세요."
+          required
+        />
+        <Input
+          label="만남 장소"
+          name="meeting_place"
+          type="text"
+          placeholder="주소를 입력해주세요."
+          required
+        />
+        <Input
+          label="시간"
+          name="meeting_time"
+          type="datetime-local"
+          required
+        />
+
+        <Button type="submit">작성완료</Button>
+      </form>
+    </main>
+  );
 }
