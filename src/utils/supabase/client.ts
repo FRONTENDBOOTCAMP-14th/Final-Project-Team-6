ac228f4b261df4736090eb879d63ed76e68a3507
Jandi95 <dduk2684@naver.com>
2025-10-13 09:27:42 +0900

    [feat] : 서버측 인증 데이터의 타입 적용 및 기존 libs 속의 코드 제거

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,4 +1,5 @@
 import { createBrowserClient } from "@supabase/ssr";
+import type { Database } from "./database.types";
 
 export function createClient() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -8,5 +9,5 @@ export function createClient() {
     throw new Error("Supabase 환경 변수가 설정되지 않았습니다.");
   }
 
-  return createBrowserClient(url, key);
+  return createBrowserClient<Database>(url, key);
 }
