87e4cdce0f03332a0e92258fcce7c8693bdeffed
Jandi95 <dduk2684@naver.com>
2025-10-14 17:08:37 +0900

    [feat] : 메세지 리스트업 및 게시글 이동 기능 개발

---
+++ src/app/chat/detail/[matchedId]/components/index.ts
@@ -0,0 +1,5 @@
+export { default as ChatDetailTitle } from "./chat-detail-title";
+export { default as Message } from "./message";
+export { default as MsgList } from "./msg-list";
+export { default as PostLink } from "./post-link";
+export { default as SendMessage } from "./send-message";
---
+++ src/app/chat/detail/[matchedId]/page.tsx
@@ -0,0 +1,49 @@
+import { getCurrentUser } from "@/utils/supabase/get-current-user";
+import { createClient } from "@/utils/supabase/server";
+import { MsgList, PostLink, SendMessage } from "./components";
+
+interface Props {
+  params: Promise<{ matchedId: string }>;
+}
+
+export default async function ChatDetailPage({ params }: Props) {
+  const { matchedId } = await params;
+
+  const supabase = await createClient();
+
+  const currentUser = await getCurrentUser();
+  if (currentUser === null) throw new Error("로그인이 필요합니다.");
+
+  const { data: chatRoomData, error: chatRoomError } = await supabase
+    .from("chat_rooms")
+    .select(`
+        chat_messages(
+          id,
+          body,
+          created_at,
+          sender_id,
+          profiles (
+            nickname,
+            profile_image_url,
+            runner_type
+          )
+        ),
+        posts(description, goal_km, meeting_place, meeting_time)
+      `)
+    .eq("matches_id", matchedId)
+    .single();
+  if (chatRoomError) throw new Error("메세지를 불러오기 오류");
+
+  console.log(chatRoomData);
+
+  const messagesData = chatRoomData.chat_messages;
+  const postData = chatRoomData.posts;
+
+  return (
+    <div className="pt-20">
+      <PostLink postData={postData} />
+      <MsgList messagesData={messagesData} currentUserId={currentUser.id} />
+      <SendMessage />
+    </div>
+  );
+}
