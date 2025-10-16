44c7f97b08a6c766f7d41466d99df4afc05374d6
Jandi95 <dduk2684@naver.com>
2025-10-16 17:25:59 +0900

    [reafactor] : 공통 input 컴포넌트 id 값 api 적용

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -1,5 +1,5 @@
 // 인풋 컴포넌트에선 :focus 같은 CSS 상태를 useState 없이 간결하게 관리하기 위해 className(global.css)사용했습니다.
-import { type InputHTMLAttributes, useId } from "react";
+import type { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -17,7 +17,7 @@ export default function Input({
   suffixIcon,
   ...props
 }: InputProps) {
-  const id = useId();
+  const id = crypto.randomUUID();
 
   const borderColor = isError
     ? "border-[var(--color-site-red)]"
