cba16a7ed7f5faa722ea935be05d3901209453d4
hunky1st <hunky1st@naver.com>
2025-10-20 13:05:28 +0900

    [feat] : googlemaps를 이용한 정적 지도 출력

--- src/app/post/detail/_components/post-info.tsx
+++ src/app/post/detail/_components/post-info.tsx
@@ -5,9 +5,15 @@ interface PostInfoProps {
   post: PostWithAuthor;
 }
 
-function InfoItem({ label, value }: { label: string; value: string | number }) {
+function InfoItem({
+  label,
+  value,
+}: {
+  label: string | number;
+  value: string | number;
+}) {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col gap-[4px]">
       <span className="text-[var(--color-site-gray)] text-sm">{label}</span>
       <span className="font-semibold text-[var(--color-site-white)] text-base">
         {value}
@@ -17,12 +23,27 @@ function InfoItem({ label, value }: { label: string; value: string | number }) {
 }
 
 export default function PostInfo({ post }: PostInfoProps) {
+  const address = post.meeting_place;
+  const encodedAddress = encodeURIComponent(address);
+  const mapSrc = `https://maps.google.com/maps?q=${encodedAddress}&output=embed`;
+
   return (
     <>
       <h2 className="text-2xl font-bold">{post.title}</h2>
-      <div className="w-full aspect-video bg-[var(--color-site-lightblack)] rounded-lg flex items-center justify-center">
-        <p className="text-[var(--color-site-gray)]">지도 영역</p>
+
+      <div className="w-full aspect-video bg-[var(--color-site-lightblack)] rounded-lg overflow-hidden">
+        <iframe
+          title="러닝 약속 장소"
+          src={mapSrc}
+          width="100%"
+          height="100%"
+          style={{ border: 0 }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
+
       <div className="flex flex-col gap-4 text-sm">
         <InfoItem label="러닝 희망 장소" value={post.meeting_place} />
         <InfoItem
@@ -30,7 +51,10 @@ export default function PostInfo({ post }: PostInfoProps) {
           value={formatUTCtoKST(post.meeting_time)}
         />
         <InfoItem label="목표 거리" value={`${post.goal_km}km`} />
-        <InfoItem label="목표 페이스" value={`${post.pace}분/km`} />
+        <InfoItem
+          label="목표 페이스"
+          value={`${Math.floor(post.pace / 60)}분 ${post.pace % 60}초/km`}
+        />
       </div>
       <p className="text-base leading-relaxed text-[var(--color-site-gray)]">
         {post.description}
