127eba3de7ed14f2d9b1551a2bd2fbc764d51cc8
hunky1st <hunky1st@naver.com>
2025-10-21 00:26:22 +0900

    [fix] : 프로필컴포넌트에서 이미지가 헤더를 뚫고 나오는 문제 수정

--- src/app/post/detail/_components/author-profile.tsx
+++ src/app/post/detail/_components/author-profile.tsx
@@ -1,16 +1,32 @@
 import Image from "next/image";
 import type { PostWithAuthor } from "@/app/post/type";
+import RunnerTypeBadge from "@/components/common/runner-type-badge";
 
 interface AuthorProfileProps {
   author: PostWithAuthor["author"];
+  created_at?: string;
 }
 
-export default function AuthorProfile({ author }: AuthorProfileProps) {
+export default function AuthorProfile({
+  author,
+  created_at,
+}: AuthorProfileProps) {
   if (!author) return null;
+
+  const formatDate = (dateString: string) => {
+    return new Date(dateString)
+      .toLocaleDateString("ko-KR", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      })
+      .replace(/\s/g, "")
+      .slice(0, -1);
+  };
 
   return (
     <div className="flex items-center gap-3">
-      <div className="relative w-12 h-12 overflow-hidden rounded-full">
+      <div className="z-0 relative w-12 h-12 overflow-hidden rounded-full">
         <Image
           src="/assets/default-profile.png"
           alt={`${author.nickname}님의 프로필 사진`}
@@ -19,15 +35,20 @@ export default function AuthorProfile({ author }: AuthorProfileProps) {
           sizes="40px"
         />
       </div>
-      <div>
-        <p className="font-bold text-[var(--color-site-white)]">
-          {author.nickname}
-        </p>
-        <p className="text-sm text-[var(--color-site-gray)]">
-          {author.runner_type === "guide_runner"
-            ? "가이드러너"
-            : "시각장애인 러너"}
-        </p>
+
+      <div className="flex flex-col gap-1">
+        <div className="flex items-center gap-2">
+          <p className="font-bold text-[var(--color-site-white)]">
+            {author.nickname}
+          </p>
+          <RunnerTypeBadge runnerType={author.runner_type} />
+        </div>
+
+        {created_at && (
+          <p className="text-sm text-[var(--color-site-gray)]">
+            작성일 {formatDate(created_at)}
+          </p>
+        )}
       </div>
     </div>
   );
