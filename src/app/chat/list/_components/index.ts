baf9235a891e0d0325a5e7995a960d3e3202ed6d
jeonjyeon <peacheese17@gmail.com>
2025-10-22 09:30:43 +0900

    [feat] : componets index.ts 파일 생성

---
+++ src/app/chat/list/_components/index.ts
@@ -0,0 +1,4 @@
+export { default as ChatItem } from "./chat-item";
+export { default as ChatListContent } from "./chat-list-content";
+export { default as ChatListRealTime } from "./chat-list-real-time";
+export { default as EmptyChatList } from "./empty-chat-list";
