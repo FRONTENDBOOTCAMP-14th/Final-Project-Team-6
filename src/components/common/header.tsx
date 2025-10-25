fb38ed58aac33d00f05acd78bb74ee3ceb16ecfe
jeonjyeon <peacheese17@gmail.com>
2025-10-26 02:03:23 +0900

    [refactor] : 설정 버튼 대신 사용자 정보 및 표시하도록 수정 - PageLayout에서 getCurrentUser()와 프로필 데이터 조회 - Header에 로그인 상태별 UI 분기 처리 (로그인/비로그인) - 로그인 시 닉네임과 프로필 이미지 표시(프로필 이미지 클릭 -> 프로필 페이지로 이동) - 비로그인 시 로그인 링크 제공(로고 이미지 클릭 -> 로그인 페이지로 이동)

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,7 +1,43 @@
-import IconSettings from "@/components/common/icons/icon-settings";
+import type { User } from "@supabase/supabase-js";
+import Image from "next/image";
 import Link from "./link";
 
-export default function Header() {
+interface HeaderProps {
+  user: User | null;
+  profile: { profile_image_url: string | null; nickname: string | null } | null;
+}
+
+export default async function Header({ user, profile }: HeaderProps) {
+  const nickname = profile?.nickname || "눈길 사용자";
+  const profileImage = profile?.profile_image_url || "default-profile.png";
+
+  if (!user) {
+    return (
+      <header className="fixed w-full h-[70px] top-0 z-50 bg-[var(--color-site-black)]  border-b border-[var(--color-site-lightblack)] max-w-(--viewport-size)">
+        <div className="h-full flex items-center justify-between px-[20px]">
+          <Link href="/">
+            <h1 className="text-xl font-bold text-[var(--color-site-white)]">
+              눈길
+            </h1>
+          </Link>
+
+          <div className="flex items-center gap-2.5">
+            <span className="text-xs">로그인이 필요합니다.</span>
+            <Link href={"/auth/login"}>
+              <Image
+                src="/images/logo.png"
+                alt=""
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        </div>
+      </header>
+    );
+  }
+
   return (
     <header className="fixed w-full h-[70px] top-0 z-50 bg-[var(--color-site-black)]  border-b border-[var(--color-site-lightblack)] max-w-(--viewport-size)">
       <div className="h-full flex items-center justify-between px-[20px]">
@@ -11,13 +47,21 @@ export default function Header() {
           </h1>
         </Link>
 
-        <button
-          type="button"
-          aria-label="설정 페이지로 이동"
-          className="w-[40px] h-[40px] flex items-center justify-center cursor-pointer"
-        >
-          <IconSettings />
-        </button>
+        <div className="flex items-center gap-2.5">
+          <span className="text-xs">
+            환영합니다,{" "}
+            <span className="text-site-blue font-bold">{nickname}</span>님!
+          </span>
+          <Link href={"/profile/my-profile"}>
+            <Image
+              src={`/images/${profileImage}`}
+              alt=""
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          </Link>
+        </div>
       </div>
     </header>
   );
