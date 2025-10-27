054802e98dd80c1a690c134be9692b7f15d1de17
Jandi95 <dduk2684@naver.com>
2025-10-27 17:18:19 +0900

    [style] : 에러 컴포넌트 스타일링 수정

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,12 +11,12 @@ export default function GlobalError({ error }: Props) {
   return (
     <div
       role="alert"
-      className="h-[100dvh] flex flex-col items-center justify-center"
+      className="h-[100dvh] flex flex-col items-center justify-center px-5"
     >
       <h1 className="text-2xl font-bold mb-3 text-center">
         페이지를 표시할 수 없습니다.
       </h1>
-      <p className="text-site-gray mb-10">{error.message}</p>
+      <p className="text-site-gray mb-10 text-center">{error.message}</p>
       <Link
         href="/"
         className="flex items-center gap-2 bg-site-blue h-[40px] px-4 rounded-sm"
